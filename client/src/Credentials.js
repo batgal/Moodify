dcceb4186a3c1968a6ee7a31acf4d211bca3680e
Villette Comfort <villette@live.com>
2022-10-10 19:05:52 -0700

    Api is working, need to fix .env to hide clientid and secret

--- client/src/Credentials.js
+++ client/src/Credentials.js
@@ -1,8 +1,11 @@
 const Credentials = () => {
 
     return {
-        ClientId: process.env.CLIENTID,
-        ClientSecret: process.env.CLIENTSECRET
+        // ClientId: process.env.CLIENTID,
+        // ClientSecret: process.env.CLIENTSECRET
+
+        ClientId: '9f979711dc7841799896506d4378e492',
+        ClientSecret: 'aa62bd6d0f0c4d25aaa12f50e9b1a678'
     }
 }
 
