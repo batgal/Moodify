ea11be82c0d3abb1e855dd4209cd4f87bb0fcc85
KatlynGreen <batgalkgg@gmail.com>
2022-10-15 16:05:25 -0400

    made adjustments to the mongo uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/moody", {
   useUnifiedTopology: true,
 });
 
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
